b95a769f7be7f5dc0e7ff82cd52544fd91da44a4
joe <joe>
2017-10-06 12:45:26 +0530

    fix the image issue in the forgot password in webservice

--- resources/views/email/forgot-password.php
+++ resources/views/email/forgot-password.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<link rel="stylesheet" href="../../../1.0.0/public/css/bootstrap.css" />
+<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/css/bootstrap.css" />
 </head>
 <body>
 <div class="container">
   <div style="border:solid 1px #999999; position: relative;    left: 14.4em; width: 60% !important; border:1px solid #000; background-color:#fff; border-collapse:collapse;" align="center">
-    <div style="background: #1fb5ad none repeat scroll 0 0; min-height:70px;"> <img  src="../../../1.0.0/public/images/logocrop.png"  style="width: 170px; " alt="KranQ" title="KranQ"></div>
+    <div style="background: #1fb5ad none repeat scroll 0 0; min-height:70px;"> <img  src="<?php echo URL::to('/'); ?>/images/logocrop.png"  style="width: 170px; " alt="KranQ" title="KranQ"></div>
     <section class="panel">
       <header class="panel-heading"><b><span class="tex-move-in" style="margin:0px 10px !important;">Reset Password</span></b> </header>
        <div class="panel-body clearfix">
