00b06a56b57a3214a5fdeae40f2309aec6d88f24
joe <joe>
2017-10-26 15:25:39 +0530

    menu Modified

--- resources/views/layouts/site_menu.blade.php
+++ resources/views/layouts/site_menu.blade.php
@@ -4,7 +4,7 @@
 		<h1><a href="{{ URL::to('/') }}/site/index">KranQ</a></h1>
 		<nav>
 			<ul class="menu">
-				<li><a class="<?php if($currentPage =='Site'){echo 'active';}?>" href="{{ URL::to('/') }}/site/index" title="Home">Home</a></li>
+				<li><a class="<?php if($currentPage =='site'){echo 'active';}?>" href="{{ URL::to('/') }}/site/index" title="Home">Home</a></li>
 				<li><a href="{{ URL::to('services') }}" class="<?php if($currentPage =='services'){echo 'active';}?>" title="Services">Services</a></li>
 				<li><a href="{{ URL::to('contact') }}" class="<?php if($currentPage =='contact'){echo 'active';}?>" title="Contact Us">Contact Us</a></li>
 			</ul>
